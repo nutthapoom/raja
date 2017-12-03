69a41a5562bb92b8972ef1cfe2e6c03a568e08dc
Big Nutthapoom <nutthapoom@digio.co.th>
2017-12-03 22:49:39 +0700

    Donate

---
+++ front/howto-donate.php
@@ -0,0 +1,325 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Donation || Responsive HTML 5 Template</title>
+
+    <!-- mobile responsive meta -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/responsive.css">
+
+    <link rel="apple-touch-icon" sizes="180x180" href="images/favicons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" href="images/favicons/favicon-32x32.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="images/favicons/favicon-16x16.png" sizes="16x16">
+
+
+
+</head>
+<body>
+
+<div class="boxed_wrapper">
+
+  <div class="boxed_wrapper">
+    <?php
+    	include "./temp/navbar.html";
+    ?>
+  </div>
+<div class="breadcumb-wrapper">
+    <div class="container">
+        <div class="pull-left">
+            <ul class="list-inline link-list">
+                <li>
+                    <a href="index.php">หน้าเเรก</a>
+                </li>
+                <li>
+                    <a href="contact.html#">เกี่ยวกับมูลนิธิ</a>
+                </li>
+
+                <li>
+                    	วิธีการบริจาค
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
+
+<section class="team-style-1 sec-padd2">
+  <div class="container">
+    <div class="section-title center">
+      <h2>วิธีการ <span class="thm-color">บริจาค</span></h2>
+
+
+
+    </div>
+  </div>
+</section>
+
+
+
+<div class="row">
+    <div class="col-sm-2"></div>
+    <div class="col-sm-8" >
+     <p>1. บริจาคได้โดยตรงที <strong>ผู้อำนวยการโรงพยาบาลราชวิถี, ประธานมูลนิธิราชวิถี </strong>เบอร์โทร. 0-2354-8138 ต่อ 3217-9<br><br>
+     2. บริจาค ณ จุดรับบริจาคมูลนิธิโรงพยาบาลราชวิถี ชั้น 1 ตึกสิริธร โรงพยาบาลราชวิถี
+     <br><br>
+     3. บริจาคผ่านทางเคาน์เตอร์ธนาคาร ชื่อบัญชี"มูลนิธิโรงพยาบาลราชงวิถี" ดังนี้
+     <br><br>
+     <div class="row">
+     <div class="col-sm-1"></div>
+      <div class="col-sm-10"style="background-color:lavender;">
+        <div class="row">
+          <div class="col-sm-4" style="background-color:lavender;">
+            <p><br>&emsp;&emsp; - ธนาคาร ไทยพาณิชย์ จำกัด<br>
+              &emsp;&emsp; - ธนาคาร กสิกรไทย จำกัด<br>
+              &emsp;&emsp; - ธนาคาร กรุงเทพ จำกัด<br>
+              &emsp;&emsp; - ธนาคาร กรุงไทย จำกัด<br>
+              &emsp;&emsp; - ธนาคาร ออมสิน จำกัด<br>
+              &emsp;&emsp; - ธนาคาร กรุงศรีอยุธยา จำกัด<br><br></p>
+          </div>
+          <div class="col-sm-4" style="background-color:lavender;">
+            <p><br>สาขาโรงพยาบาลราชวิถี<br>
+              สาขาสนามเป้า<br>
+              สาขาอาคารยาคูลย์<br>
+              สาขาถนนศรีอยุธยา<br>
+              สาขาสำนักงานใหญ่<br>
+              สาขาราชเทวี</p>
+          </div>
+          <div class="col-sm-4" style="background-color:lavender;">
+            <p><br>บัญชีเลขที่ 051-2-16322-1<br>
+              บัญชีเลขที่ 029-2-60574-9<br>
+              บัญชีเลขที่ 210-0-56926-4<br>
+              บัญชีเลขที่ 013-1-77009-8<br>
+              บัญชีเลขที่ 05001228487-8<br>
+              บัญชีเลขที่ 044-1-34621-7</p></div>
+        </div>
+
+
+      </div>
+     <div class="col-sm-1" ></div>
+     </div>
+     </p>
+     <br><br>
+     <p>
+     4. บริจาคโดยเช็คบุคคล หรือเช็คธนาคาร สั่งจ่าย <strong>"มูลนิธิโรงพยาบาลราชวิถี"</strong> ขีดคร่อม A/C PAYEE ONLY และ "หรือผู้ถือ" <br>&emsp;ธนาคาร..................หมายเลขเช็ค..................
+     <br><br>
+     5. บริจาคโดยธนาณัติ สั่งจ่าย <strong>"มูลนิธิโรงพยาบาลราชวิถี ปณ.ราชเทวี 10401"</strong>
+     </p>
+     <br><br>
+     <!-- <div class="row">
+    <div class="col-sm-1" ></div> -->
+    <div class="col-sm-12" style="background-color:lavenderblush;">
+      <br>
+      <p>
+      &emsp;&emsp;(กรณีโอนเงินบริจาคผ่านะนาคาร ขอใหเส่งสำเนาสลิปฝากเงินได้ที่ <strong>เบอร์แฟกซ์ 02-354-7996 หรือส่ง E-mail : rejavithihospitalfoundation@gmail.com</strong>
+       ระบุชื่อ-นามสกุลผู้บริจาค พร้อมที่อยู่ที่สามารถติดต่อได้ เเละระบุวัตถุประสงค์ของการบริจาค เพื่อมูลนิธิฯ จะได้จัดส่งใบเสร็จรับเงินให้ท่าน เพื่อนำไปลดหย่อนภาษีได้ประจำปีนั้น)
+      </p>
+     <br>
+     </div>
+      <!-- <div class="col-sm-1"></div>
+      </div> -->
+
+      <div style="text-align:center">
+          <img src="../images/history/his3.jpg" style="">
+      </div>
+
+
+    </div>
+
+    <div class="col-sm-2" ></div>
+</div>
+
+
+
+
+
+
+
+
+
+<br>
+
+<!--Footer Bottom-->
+ <section class="footer-bottom">
+    <div class="container">
+        <p class="center"><a href="contact.html#">Copyrights © 2017</a> All Rights Reserved. Powered by <a href="contact.html#">Donation</a></p>
+    </div><!-- /.container -->
+</section>
+
+    <!-- Scroll Top  -->
+    <button class="scroll-top tran3s color2_bg"><span class="fa fa-angle-up"></span></button>
+    <!-- preloader  -->
+    <div class="preloader"></div>
+    <div id="donate-popup" class="donate-popup">
+    <div class="close-donate theme-btn"><span class="fa fa-close"></span></div>
+    <div class="popup-inner">
+
+
+    <div class="container">
+        <div class="donate-form-area">
+            <div class="section-title center">
+                <h2>Donation Information</h2>
+            </div>
+
+            <h4>How much would you like to donate:</h4>
+
+            <form  action="contact.html#" class="donate-form default-form">
+                <ul class="chicklet-list clearfix">
+                    <li>
+                        <input type="radio" id="donate-amount-1" name="donate-amount" />
+                        <label for="donate-amount-1" data-amount="1000" >$1000</label>
+                    </li>
+                    <li>
+                        <input type="radio" id="donate-amount-2" name="donate-amount" checked="checked" />
+                        <label for="donate-amount-2" data-amount="2000">$2000</label>
+                    </li>
+                    <li>
+                        <input type="radio" id="donate-amount-3" name="donate-amount" />
+                        <label for="donate-amount-3" data-amount="3000">$3000</label>
+                    </li>
+                    <li>
+                        <input type="radio" id="donate-amount-4" name="donate-amount" />
+                        <label for="donate-amount-4" data-amount="4000">$4000</label>
+                    </li>
+                    <li>
+                        <input type="radio" id="donate-amount-5" name="donate-amount" />
+                        <label for="donate-amount-5" data-amount="5000">$5000</label>
+                    </li>
+                    <li class="other-amount">
+
+                        <div class="input-container" data-message="Every dollar you donate helps end hunger.">
+                            <span>Or</span><input type="text" id="other-amount" placeholder="Other Amount"  />
+                        </div>
+                    </li>
+                </ul>
+
+                <h3>Donor Information</h3>
+
+                <div class="form-bg">
+                    <div class="row clearfix">
+                        <div class="col-md-6 col-sm-6 col-xs-12">
+
+                            <div class="form-group">
+                                <input type="text" name="fname" placeholder="First Name">
+                            </div>
+                        </div>
+                        <div class="col-md-6 col-sm-6 col-xs-12">
+
+                            <div class="form-group">
+                                <input type="text" name="fname" placeholder="Last Name">
+                            </div>
+                        </div>
+                        <div class="col-md-6 col-sm-6 col-xs-12">
+                            <div class="form-group">
+                                <input type="text" name="fname" placeholder="Email">
+                            </div>
+                        </div>
+                        <div class="col-md-6 col-sm-6 col-xs-12">
+                            <div class="form-group">
+                                <input type="text" name="fname" placeholder="Address">
+                            </div>
+                        </div>
+                        <div class="col-md-6 col-sm-6 col-xs-12">
+                            <div class="form-group">
+                                <input type="text" name="fname" placeholder="Phone">
+                            </div>
+                        </div>
+                        <div class="col-md-6 col-sm-6 col-xs-12">
+                            <div class="form-group">
+                                <input type="text" name="fname" placeholder="Additional Note">
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+                <ul class="payment-option">
+                    <li>
+                        <h4>Choose your payment method:</h4>
+                    </li>
+                    <li>
+                        <div class="checkbox">
+                            <label>
+                                <input name="pay-us" type="checkbox">
+                                <span>Paypal</span>
+                            </label>
+                        </div>
+                    </li>
+                    <li>
+                       <div class="checkbox">
+                            <label>
+                                <input name="pay-us" type="checkbox">
+                                <span>Offline Donation</span>
+                            </label>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="checkbox">
+                            <label>
+                                <input name="pay-us" type="checkbox">
+                                <span>Credit Card</span>
+                            </label>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="checkbox">
+                            <label>
+                                <input name="pay-us" type="checkbox">
+                                <span>Debit Card</span>
+                            </label>
+                        </div>
+                    </li>
+                </ul>
+
+                <div class="center"><button class="thm-btn" type="submit">Donate Now</button></div>
+
+
+            </form>
+        </div>
+    </div>
+
+
+
+    </div>
+</div>
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/menu.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/jquery.mixitup.min.js"></script>
+    <script src="js/jquery.fancybox.pack.js"></script>
+    <script src="js/imagezoom.js"></script>
+    <script src="js/jquery.magnific-popup.min.js"></script>
+    <script src="js/jquery.polyglot.language.switcher.js"></script>
+    <script src="js/SmoothScroll.js"></script>
+    <script src="js/jquery.appear.js"></script>
+    <script src="js/jquery.countTo.js"></script>
+    <script src="js/validation.js"></script>
+    <script src="js/wow.js"></script>
+    <script src="js/jquery.fitvids.js"></script>
+    <script src="js/nouislider.js"></script>
+    <script src="js/isotope.js"></script>
+    <script src="js/jquery.bootstrap-touchspin.js"></script>
+    <script src="js/jquery-ui.js"></script>
+    <script src="js/bootstrap-select.min.js"></script>
+
+
+    <!-- google map js -->
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCRvBPo3-t31YFk588DpMYS6EqKf-oGBSI"></script>
+    <script src="js/gmap.js"></script>
+
+    <script id="map-script" src="js/default-map.js"></script>
+
+
+
+    <script src="js/custom.js"></script>
+
+</div>
+
+</body>
+</html>
