5ea492a6585b42e894e929b9bbe8554e7d2a3f03
Big Nutthapoom <nutthapoom@digio.co.th>
2017-12-03 12:15:35 +0700

    Edit nabber and add news

--- front/news-details.php
+++ front/news-details.php
@@ -16,6 +16,8 @@
     <link rel="icon" type="image/png" href="../images/favicons/favicon-32x32.png" sizes="32x32">
     <link rel="icon" type="image/png" href="../images/favicons/favicon-16x16.png" sizes="16x16">
 
+
+
 </head>
 <body>
 
@@ -23,13 +25,6 @@
   <?php
   	include "./temp/navbar.html";
   ?>
-<!-- <div class="inner-banner has-base-color-overlay text-center" style="background: url(../images/background/4.jpg);">
-    <div class="container">
-        <div class="box">
-            <h1>Blog Grid View</h1>
-        </div>
-    </div>
-</div> -->
 <div class="breadcumb-wrapper">
     <div class="container">
         <div class="pull-left">
@@ -38,7 +33,7 @@
                     <a href="index.html">หน้าหลัก</a>
                 </li>
                 <li>
-                    ข่าวประชาสัมพันธ์
+                    รายละเอียดข่าว
                 </li>
             </ul>
         </div>
@@ -46,72 +41,97 @@
 </div>
 
 
-
-
-<section class="all-blog blog-section sec-padd2">
+<section class="blog-single-post blog-section">
     <div class="container">
-
         <div class="row">
-            <article class="col-md-4 col-sm-6 col-xs-12">
-                <div class="default-blog-news">
-                    <figure class="img-holder">
-                        <a href="./news-details.php"><img src="../images/blog/1.jpg" alt="News"></a>
-                        <div class="inner-box">
+            <div class="col-md-8 col-sm-12">
+                <div class="post-area sec-padd">
+                    <article class="classic-blog-news">
+                        <figure class="img-holder">
+                            <a href="blog-details.html"><img src="../images/blog/l2.jpg" alt="News"></a>
+                            <div class="inner-box">
 
-                        </div>
+                            </div>
 
-                    </figure>
-                    <div class="lower-content">
-                        <div class="category">Jan 05, 2017</div>
-                        <div class="content">
-                            <a href="./news-details.php"><h4>Education For All Campaign</h4></a>
-                            <div class="text"><p>Reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.</p></div>
+                        </figure>
+                        <div class="lower-content">
+                            <div class="category">Mar 12, 2017</div>
+                            <div class="content">
+                                <div class="post-meta">By jacob  |  10 Comments</div>
+                                <a href="blog-details.html"><h4>onation food for childrens</h4></a>
+                                <p>Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                                <div class="text">
+                                    <p>Nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                                    <p>Audiam virtute ut, case utamur fuisset eam ut, iisque accommodare an eam. Reque blandit qui eu, cu vix nonumy volumus. Legendos intellegam id usu, vide oporteat vix eu, id illud principes has. Nam tempor utamur gubergren no. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                                </div>
+                                <div class="author">
+                                    <!-- Image -->
+                                    <img src="../images/blog/author.jpg" alt="">
+                                    <div class="author-comment">
+                                        <div class="quote">
+                                            <i class="fa fa-quote-left"></i>
+                                        </div>
+                                        <p>Ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                                        <h5>brendan Taylor</h5>
+                                        <p class="a-title">Manager</p>
+                                    </div>
+                                </div>
+                                <div class="text">
+                                    <p>Know how to pursue pleasure rationally encounter consequences that are extremely painful seds or again is there anyone who loves or pursues or desires to obtain pain of itself, because seds our it pain, but because occasionally circumstances occur which toil and pain can  undertakes laborious physical exercise.</p>
+                                </div>
+                                <div class="share-box clearfix">
+                                    <ul class="tag-box pull-left">
+                                        <li>Category: </li>
+                                        <li><a href="blog-details.html#">Donation,</a></li>
+                                        <li><a href="blog-details.html#">Childrens,</a></li>
+                                        <li><a href="blog-details.html#">Education,</a></li>
+                                    </ul>
+                                    <div class="social-box pull-right">
+                                    <span>Share <i class="fa fa-share-alt"></i></span>
+                                    <ul class="list-inline social">
+                                            <li><a href="blog-details.html#"><i class="fa fa-facebook"></i></a></li>
+                                            <li><a href="blog-details.html#"><i class="fa fa-twitter"></i></a></li>
+                                            <li><a href="blog-details.html#"><i class="fa fa-google-plus"></i></a></li>
+                                            <li><a href="blog-details.html#"><i class="fa fa-pinterest"></i></a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                    </div>
+                    </article>
                 </div>
 
-            </article>
-            <article class="col-md-4 col-sm-6 col-xs-12">
-                <div class="default-blog-news">
-                    <figure class="img-holder">
-                        <a href="./news-details.php"><img src="../images/blog/2.jpg" alt="News"></a>
-                        <div class="inner-box">
-
+            </div>
+            <div class="col-md-4 col-sm-12">
+                <div class="blog-sidebar sec-padd">
+                    <div class="popular_news">
+                        <div class="section-title style-2">
+                            <h4>ข่าวอื่นที่น่าสนใจ</h4>
                         </div>
 
-                    </figure>
-                    <div class="lower-content">
-                        <div class="category">Feb 08, 2017</div>
-                        <div class="content">
-                            <a href="./news-details.php"><h4>Donation food for childrens</h4></a>
-                            <div class="text"><p>consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p></div>
-                        </div>
-                    </div>
-                </div>
-
-            </article>
-            <article class="col-md-4 col-sm-6 col-xs-12">
-                <div class="default-blog-news">
-                    <figure class="img-holder">
-                        <a href="./news-details.php"><img src="../images/blog/3.jpg" alt="News"></a>
-                        <div class="inner-box">
-
-                        </div>
-
-                    </figure>
-                    <div class="lower-content">
-                        <div class="category">Mar 12, 2017</div>
-                        <div class="content">
-                            <a href="./news-details.php"><h4>Helping Kids Grow Up Stronger</h4></a>
-                            <div class="text"><p>Soleat habemus usu, te nec eligendi deserunt vituperata. Natum consulatu vel ea, duo cetero repudiare efficiendi cu.</p></div>
+                        <div class="popular-post">
+                            <div class="item">
+                                <div class="post-thumb"><a href="blog-details.html"><img src="../images/blog/thumb3.jpg" alt=""></a></div>
+                                <a href="blog-details.html"><h4>New Award To Support <br>Children Affected by Crises</h4></a>
+                                <div class="post-info">October 21, 2016</div>
+                            </div>
+                            <div class="item">
+                                <div class="post-thumb"><a href="blog-details.html"><img src="../images/blog/thumb4.jpg" alt=""></a></div>
+                                <a href="blog-details.html"><h4>Help promote girl's Education <br>in the world</h4></a>
+                                <div class="post-info">January 14, 2016</div>
+                            </div>
+                            <div class="item">
+                                <div class="post-thumb"><a href="blog-details.html"><img src="../images/blog/thumb5.jpg" alt=""></a></div>
+                                <a href="blog-details.html"><h4>Clean poor urban areas to <br>protect from pollution.</h4></a>
+                                <div class="post-info">December 17, 2015</div>
+                            </div>
                         </div>
                     </div>
+
+
+
                 </div>
-
-            </article>
-
-
-
+            </div>
 
         </div>
     </div>
@@ -120,7 +140,7 @@
 <!--Footer Bottom-->
  <section class="footer-bottom">
     <div class="container">
-        <p class="center"><a href="blog-1.html#">Copyrights © 2017</a> All Rights Reserved. Powered by <a href="blog-1.html#">Donation</a></p>
+        <p class="center"><a href="blog-details.html#">Copyrights © 2017</a> All Rights Reserved. Powered by <a href="blog-details.html#">Donation</a></p>
     </div><!-- /.container -->
 </section>
 
@@ -141,7 +161,7 @@
 
             <h4>How much would you like to donate:</h4>
 
-            <form  action="blog-1.html#" class="donate-form default-form">
+            <form  action="blog-details.html#" class="donate-form default-form">
                 <ul class="chicklet-list clearfix">
                     <li>
                         <input type="radio" id="donate-amount-1" name="donate-amount" />
